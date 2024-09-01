da95fff04a82530466b8f8f4b7d0ab1afcdb0a0f
Decathlon Egypt <159443038+Decathlon-egypt@users.noreply.github.com>
2024-09-01 13:47:15 +0300

    Update Showroom.js

--- CMS Scripts & Styles/Showroom.js
+++ CMS Scripts & Styles/Showroom.js
@@ -1,86 +1,63 @@
 document.addEventListener("DOMContentLoaded", function() {
-const tracks = document.querySelectorAll(".showroom-products-track");
-const leftshowroom_arrows = document.querySelectorAll(".left_showroom_arrow");
-const rightshowroom_arrows = document.querySelectorAll(".right_showroom_arrow");
+  const tracks = document.querySelectorAll(".showroom-products-track");
+  const leftShowroomArrows = document.querySelectorAll(".left_showroom_arrow");
+  const rightShowroomArrows = document.querySelectorAll(".right_showroom_arrow");
+  const trackTranslations = Array(tracks.length).fill(0);
 
-const trackTranslations = Array(tracks.length).fill(0);
-
-const translateContainer = (track, index, amount) => {
-  const displayWidth = window.innerWidth - 419;
-  const trackWidth = track.offsetWidth;
-  let maxTranslateX = trackWidth - displayWidth;
-
-  trackTranslations[index] += amount;
-
-  if (trackTranslations[index] >= 0) {
-    trackTranslations[index] = 0;
-  } else if (trackTranslations[index] < -maxTranslateX) {
-    trackTranslations[index] = -maxTranslateX;
-  }
-
-  track.style.transform = `translateX(${trackTranslations[index]}px)`;
-  track.style.transition = "transform 0.5s ease";
-};
-
-document.addEventListener("DOMContentLoaded", function() {
   const noImageTracks = document.querySelectorAll(".no-image-track");
   const leftNoImageArrows = document.querySelectorAll(".left_no_image_arrow");
   const rightNoImageArrows = document.querySelectorAll(".right_no_image_arrow");
-
   const noImageTrackTranslations = Array(noImageTracks.length).fill(0);
 
-  const translateNoImageContainer = (track, index, amount) => {
-    const displayWidth = window.innerWidth;
+  const translateContainer = (track, index, amount, translations) => {
+    const displayWidth = window.innerWidth - 419;
     const trackWidth = track.offsetWidth;
-    let maxTranslateX = trackWidth - displayWidth;
+    const maxTranslateX = trackWidth - displayWidth;
 
-    noImageTrackTranslations[index] += amount;
+    translations[index] += amount;
 
-    if (noImageTrackTranslations[index] >= 0) {
-      noImageTrackTranslations[index] = 0;
-    } else if (noImageTrackTranslations[index] < -maxTranslateX) {
-      noImageTrackTranslations[index] = -maxTranslateX;
+    if (translations[index] >= 0) {
+      translations[index] = 0;
+    } else if (translations[index] < -maxTranslateX) {
+      translations[index] = -maxTranslateX;
     }
 
-    track.style.transform = `translateX(${noImageTrackTranslations[index]}px)`;
+    track.style.transform = `translateX(${translations[index]}px)`;
     track.style.transition = "transform 0.5s ease";
   };
 
-  leftNoImageArrows.forEach((noImageArrow, index) => {
-    noImageArrow.addEventListener("click", () => {
-      const track = noImageTracks[index];
+  leftShowroomArrows.forEach((arrow, index) => {
+    arrow.addEventListener("click", () => {
+      const track = tracks[index];
       const secondChildWidth = 261;
-      translateNoImageContainer(track, index, secondChildWidth);
+      translateContainer(track, index, secondChildWidth, trackTranslations);
     });
   });
 
-  rightNoImageArrows.forEach((noImageArrow, index) => {
-    noImageArrow.addEventListener("click", () => {
+  rightShowroomArrows.forEach((arrow, index) => {
+    arrow.addEventListener("click", () => {
+      const track = tracks[index];
+      const secondChildWidth = 261;
+      translateContainer(track, index, -secondChildWidth, trackTranslations);
+    });
+  });
+
+  leftNoImageArrows.forEach((arrow, index) => {
+    arrow.addEventListener("click", () => {
       const track = noImageTracks[index];
       const secondChildWidth = 261;
-      translateNoImageContainer(track, index, -secondChildWidth);
+      translateContainer(track, index, secondChildWidth, noImageTrackTranslations);
+    });
+  });
+
+  rightNoImageArrows.forEach((arrow, index) => {
+    arrow.addEventListener("click", () => {
+      const track = noImageTracks[index];
+      const secondChildWidth = 261;
+      translateContainer(track, index, -secondChildWidth, noImageTrackTranslations);
     });
   });
 });
-  
-leftshowroom_arrows.forEach((showroom_arrow, index) => {
-  showroom_arrow.addEventListener("click", () => {
-    const track = tracks[index];
-    const secondChildWidth = 261;
-    translateContainer(track, index, secondChildWidth);
-  });
-});
-
-rightshowroom_arrows.forEach((showroom_arrow, index) => {
-  showroom_arrow.addEventListener("click", () => {
-    const track = tracks[index];
-    const secondChildWidth = 261;
-    translateContainer(track, index, -secondChildWidth);
-  });
-});
-});
-
-
 
 const algoliaDetails = {
   app_id: algolia_user,
@@ -89,8 +66,7 @@ const algoliaDetails = {
 };
 
 function handleLoadingSliders() {
-  const loadingProducts =
-    document.getElementsByClassName("loading-products");
+  const loadingProducts = document.getElementsByClassName("loading-products");
   const loadingProductsArr = [...loadingProducts];
   loadingProductsArr.forEach((e) => {
     e.remove();
@@ -101,8 +77,7 @@ function updateImageUrl(url) {
   const newParams = "format=auto&quality=40&f=400x0";
   if (url.indexOf("?") > -1) {
     const urlParts = url.split("?");
-    let newUrl = `${urlParts[0]}?${newParams}`;
-    return newUrl;
+    return `${urlParts[0]}?${newParams}`;
   } else {
     return url;
   }
@@ -110,32 +85,32 @@ function updateImageUrl(url) {
 
 document.addEventListener("alpine:init", () => {
   Alpine.data("products", () => ({
-      products: [],
-      gridStyle: '',
-      getProductsManual(productsArr) {
-          const clientAlg = algoliasearch(
-              algoliaDetails.app_id,
-              algoliaDetails.api_search_key
-          );
-          const indexAlg = clientAlg.initIndex(algoliaDetails.index_name);
+    products: [],
+    gridStyle: '',
+    getProductsManual(productsArr) {
+      const clientAlg = algoliasearch(
+        algoliaDetails.app_id,
+        algoliaDetails.api_search_key
+      );
+      const indexAlg = clientAlg.initIndex(algoliaDetails.index_name);
 
-          const filters = productsArr.map(id => `objectID:${id}`).join(' OR ');
+      const filters = productsArr.map(id => `objectID:${id}`).join(' OR ');
 
-          indexAlg.search('', {
-              filters: filters
-          }).then(({ hits }) => {
-              handleLoadingSliders();
-              const filteredResultsManual = hits.filter((item) => item && item.stock.global);
-              this.products = filteredResultsManual;
-              this.updateGridStyle();
-          }).catch(err => {
-              console.error('Algolia Error:', err);
-          });
-      },
+      indexAlg.search('', {
+        filters: filters
+      }).then(({ hits }) => {
+        handleLoadingSliders();
+        const filteredResultsManual = hits.filter((item) => item && item.stock.global);
+        this.products = filteredResultsManual;
+        this.updateGridStyle();
+      }).catch(err => {
+        console.error('Algolia Error:', err);
+      });
+    },
 
-      updateGridStyle() {
-          const columns = this.products.length;
-          this.gridStyle = `grid-template-columns: repeat(${columns}, 1fr);`;
-      }
+    updateGridStyle() {
+      const columns = this.products.length;
+      this.gridStyle = `grid-template-columns: repeat(${columns}, 1fr);`;
+    }
   }));
 });
