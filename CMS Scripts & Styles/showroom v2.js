ec09225f4458a33fa3f3558a900f040237f0cf7a
Decathlon Egypt <159443038+Decathlon-egypt@users.noreply.github.com>
2024-11-06 16:04:09 +0200

    Update showroom v2.js

--- CMS Scripts & Styles/showroom v2.js
+++ CMS Scripts & Styles/showroom v2.js
@@ -62,7 +62,7 @@ document.addEventListener("DOMContentLoaded", function() {
 const algoliaDetails = {
   app_id: 'TR53CBEI82',
   api_search_key: "98ef65e220d8d74a2dfac7a67f1dba11",
-  index_name: algolia_index,
+  index_name: "prod_en",
 };
 
 function handleLoadingSliders() {
